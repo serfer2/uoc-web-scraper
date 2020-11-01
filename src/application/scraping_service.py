336bf131c6fa0ef209015d7f2e79643ade1a4aa3
serfer2 <serfer2@gmail.com>
2020-11-01 02:43:05 +0100

    feat(ScrapingService): added scraping common logic

--- src/application/scraping_service.py
+++ src/application/scraping_service.py
@@ -1,6 +1,39 @@
+import time
+
+import lxml.html
+
+from shared.tools import full_url
+from domain.models import Resource
+
+
 class ScrapingService:
 
-    def __init__(self, reader, repository, initial_url):
-        self.__reader = reader
-        self.__repository = repository
-        self.__initial_url = initial_url
+    def __init__(self, reader, initial_url, delay_secs=1):
+        self._reader = reader
+        self._initial_url = initial_url
+        self._delay_secs = delay_secs
+        self._urls_xpath = ''
+        self._resource_type = None
+
+    def scrape(self):
+        resources = []
+
+        for url in self.get_resources_urls(self._urls_xpath):
+            self.delay()
+
+            data = self.resource_data(self._reader.read(url))
+            data.update({'type': self._resource_type, 'url': url})
+
+            resources.append(Resource(**data))
+
+        return resources
+
+    def get_resources_urls(self, urls_xpath):
+        html = self._reader.read(self._initial_url)
+        doc = lxml.html.fromstring(html)
+
+        return [full_url(self._initial_url, url) for url in doc.xpath(urls_xpath)]
+
+    def delay(self):
+        if self._delay_secs:
+            time.sleep(self._delay_secs)
