88151933d15b4fb746d8265636cffe46916285fb
serfer2 <serfer2@gmail.com>
2020-11-01 23:05:22 +0100

    feat(UniversitaryMasterScrapingService): application service for scraping Universitary masters

--- src/application/universitary_master_scraping_service.py
+++ src/application/universitary_master_scraping_service.py
@@ -1,6 +1,29 @@
-from .scraping_service import ScrapingService
+import lxml.html
+
+from application.scraping_service import ScrapingService
+from domain.models import Resource
+
 
 class UniversitaryMasterScrapingService(ScrapingService):
-    
-    def scrape(self):
-        pass
+
+    def __init__(self, *args, **kwargs):
+        super(UniversitaryMasterScrapingService, self).__init__(*args, **kwargs)
+
+        self._resource_type = Resource.TYPE_MASTER_UNIVERSITARIO
+        self._urls_xpath = '//div[@data-type="product"]//a[@class="card-absolute-link"]/@href'
+
+    def resource_data(self, html):
+        doc = lxml.html.fromstring(html)
+
+        name = doc.xpath('//h1')[0].text_content().strip()
+        desc = doc.xpath('//section[@class="flexbox-layout m-bottom-2y uc227 is-first-unfolded"]')[0].text_content().strip()
+
+        ects, date_init, title = self.get_ects_date_init_title(html)
+
+        return {
+            'name': name,
+            'description': desc,
+            'ects': ects,
+            'date_init': date_init,
+            'title': title
+        }
